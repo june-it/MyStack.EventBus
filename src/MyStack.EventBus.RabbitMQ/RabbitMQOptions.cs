680b029513340551abb91f211fe97a58437f4e7f
June <june.it@qq.com>
2023-12-25 14:55:52 +0800

    Renaming property 'RoutingKeyPrifix'

--- src/MyStack.EventBus.RabbitMQ/RabbitMQOptions.cs
+++ src/MyStack.EventBus.RabbitMQ/RabbitMQOptions.cs
@@ -2,23 +2,54 @@
 
 namespace Microsoft.Extensions.EventBus.RabbitMQ
 {
-
+    /// <summary>
+    /// Represents a RabbitMQ configuration
+    /// </summary>
     public class RabbitMQOptions
     {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public string HostName { get; set; }
-        public int Port { get; set; }
-        public string VirtualHost { get; set; }
-        public string RoutingKeyPrifix { get; set; }
+        /// <summary>
+        /// Get or set UserName
+        /// </summary>
+        public string UserName { get; set; } = "guest";
+        /// <summary>
+        /// Get or set Password
+        /// </summary>
+        public string Password { get; set; } = "guest";
+        /// <summary>
+        /// Get or set Host name
+        /// </summary>
+        public string HostName { get; set; } = "localhost";
+        /// <summary>
+        /// Get or set Port
+        /// </summary>
+        public int Port { get; set; } = 3306;
+        /// <summary>
+        /// Get or set Virtual host
+        /// </summary>
+        public string VirtualHost { get; set; } = "/";
+        /// <summary>
+        /// Get or set Prefix for routing keys
+        /// </summary>
+        public string RoutingKeyPrefix { get; set; }
         public RabbitMQExchangeOptions ExchangeOptions { get; set; } = new RabbitMQExchangeOptions();
         public RabbitMQQueueOptions QueueOptions { get; set; } = new RabbitMQQueueOptions();
     }
-
+    /// <summary>
+    /// Represents a RabbitMQ exchange configuration
+    /// </summary>
     public class RabbitMQExchangeOptions
     {
+        /// <summary>
+        /// Get or set Exchange type
+        /// </summary>
         public string ExchangeType { get; set; }
+        /// <summary>
+        /// Get or set Exchange name
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// Get or set Exchange can be durable or transient
+        /// </summary>
         public bool Durable { get; set; }
         public bool AutoDelete { get; set; }
         public Dictionary<string, object> Arguments { get; set; }
